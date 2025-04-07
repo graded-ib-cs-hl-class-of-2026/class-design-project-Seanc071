d10c3693f57e9cda486134819af39b4a236a557c
Sean <No>
2025-04-07 12:56:58 -0300

    first class draft

---
+++ src/Book.java
@@ -0,0 +1,23 @@
+public class Book {
+    private String title;
+    private String author;
+    private boolean checkedout;
+    private String borrower;
+    
+    public Book(String title, String author, boolean checkedout, String borrower){
+        this.title = title;
+        this.author = author;
+        this.checkedout = checkedout;
+        this.borrower = borrower;
+    
+
+    }
+    public String getTitle(){
+        return title;
+    }
+    public String getAuthor(){
+        return author;
+    }
+    public boolean ge
+    
+}
